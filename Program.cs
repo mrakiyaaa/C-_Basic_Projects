9a10f93f5e67f0b0feb7b52e7bb85233af813c88
Akila_Lakshitha <136437833+mrakiyaaa@users.noreply.github.com>
2024-01-21 14:12:44 +0530

    hypotenuse calculator program
    hypotenuse calculator program in C#

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class program
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine(" Enter Side A: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine(" Enter Side b: ");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        double c = Math.Sqrt((a * a) + (b * b));
+
+        Console.WriteLine("The hypotenuse is: " + c);
+
+    }
+}
